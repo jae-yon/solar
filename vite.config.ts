c3dfce4a62741054a3d17c9c60a4e411413ad61b
JaeYoung <kimjaeyoung650@gmail.com>
2026-02-10 00:36:52 +0900

    [CHORE] 프로젝트 환경설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  preview: {
+    port: 5000,
+    host: true,
+  },
+  server: {
+    port: 5000,
+    host: true,
+  },
 })
