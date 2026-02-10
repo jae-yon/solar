de62efd7b52910e8b6fb8b7083a03c7ce457c0e6
JaeYoung <kimjaeyoung650@gmail.com>
2026-02-10 13:26:24 +0900

    [FEAT] API 연결을 위한 패키지 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,23 @@ import './index.css'
 
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1
+    }
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider value={defaultSystem}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
